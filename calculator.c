eb481d33c03d0ad5b01297fd58b2a0ed635eca94
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-11 19:24:16 +0000

    Tue, Oct 11, 2022, 7:24 PM +00:00

--- calculator.c
+++ calculator.c
@@ -3,12 +3,13 @@
 
 int main (void)
 {
+    int mine = 2;
     int points = get_int("How many points did your lose? ");
-    if (points < 2)
+    if (points < mine)
     {
         printf("youve lost viewer points than me\n");
     }
-    else if (points > 2)
+    else if (points > mine)
     {
         printf("you lost more points than me\n");
     }
