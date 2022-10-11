c15cb7d16a26aa86eccb044485fb20d8385be814
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-11 19:11:29 +0000

    Tue, Oct 11, 2022, 7:11 PM +00:00

--- calculator.c
+++ calculator.c
@@ -1,9 +1,9 @@
-#include <stdin.h>
+#include <stdio.h>
 #include <cs50.h>
 
 int main (void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
-    printf("the sum is %i/n", x + y);
+    printf("the sum is %i\n", x + y);
 }
