92438b580851520309993c941fa9e20071daed28
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-11 19:50:05 +0000

    Tue, Oct 11, 2022, 7:50 PM +00:00

---
+++ agree.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    /*prompt user to agree to something
+    char c = get_char("do you agree? ");
+    /*check wheter user agreed
+    if (c == )
+}
