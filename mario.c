36586d550ffe542b00870f960e26643e555eeb6b
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-13 13:29:02 +0000

    Thu, Oct 13, 2022, 1:29 PM +00:00

--- mario.c
+++ mario.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
+        int n;
+        do
+        {
+            n = get_int("Width: ");
+        }
+        while (n < 1);
+        
         for (int i = 0; i < 4; i++)
         {
             printf("?");
