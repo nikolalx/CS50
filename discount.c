eb57c3ea60d3c03e59ee8a9b9e6e196a3d43a8d1
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-13 13:07:09 +0000

    Thu, Oct 13, 2022, 1:07 PM +00:00

--- discount.c
+++ discount.c
@@ -1,9 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+float discount(float price);
+
 int main (void)
 {
     float regular = get_float("Regular price: ");
-    float sale = regular * .85;
+    float sale = discount(regular);
     printf("Sale Price: %.2f\n", sale);
+}
+
+float discount(float price)
+{
+    return price * .85;
 }
