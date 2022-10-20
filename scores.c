273963ef4eaa46bb549e0f3987a45684b41f26a3
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-20 22:22:46 +0000

    Thu, Oct 20, 2022, 10:22 PM +00:00

--- scores.c
+++ scores.c
@@ -12,5 +12,5 @@ int main(void)
         scores[i] = get_int("Score ");
     }
 
-    printf("Average %f\n", (scores[0] + scores[1] + scores[2]) / (float) n);
+    printf("Average %f\n", (scores[n] + scores[n]) / (float) n);
 }
