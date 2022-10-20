31146077bf1f19da9a97425cd7a371d2c0120cbd
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-20 21:07:44 +0000

    Thu, Oct 20, 2022, 9:07 PM +00:00

--- scores.c
+++ scores.c
@@ -7,7 +7,7 @@ int main(void)
     int score2 = get_int("Score for second: ");
     int score3 = get_int("Score for third: ");
 
-    float n = score1 + score2 + score3;
+    int n = score1 + score2 + score3;
 
-    printf("Average %f\n", (score1 + score2 + score3) / n);
+    printf("Average %f\n", (score1 + score2 + score3) / (float) n);
 }
