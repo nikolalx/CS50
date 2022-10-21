333e7c333e4b9ca1ecbb10c3c25809509cd6bc2e
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-21 21:20:56 +0000

    Fri, Oct 21, 2022, 9:20 PM +00:00

--- length.c
+++ length.c
@@ -5,12 +5,13 @@ int main(void)
 {
     int string_length(string s);
     string name = get_string("Name: ");
-    int i = 0;
+
     int length = string_length(name);
 }
 
 int string_length(string s)
 {
+    int i = 0;
     while (s[i] != '\0')
     {
         i++;
