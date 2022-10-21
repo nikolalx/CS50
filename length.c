d8185cac35445c72e7fd5529163cd448c95295e7
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-21 21:12:40 +0000

    Fri, Oct 21, 2022, 9:12 PM +00:00

--- length.c
+++ length.c
@@ -5,9 +5,9 @@ int main(void)
 {
     string name = get_string("Name: ");
     int i = 0;
-    while(name[i] != '\0')
+    while (name[i] != '\0')
     {
         i++;
     }
-    printf("i%\n", i);
+    printf("%i\n", i);
 }
