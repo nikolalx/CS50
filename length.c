19fa331bd1487eef2607b941b3520e241bb20510
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-21 21:25:10 +0000

    Fri, Oct 21, 2022, 9:25 PM +00:00

--- length.c
+++ length.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
-    int string_length(string s);
     string name = get_string("Name: ");
 
     int length = string_length(name);
     printf("%i\n", length);
 }
 
-int string_length(string s)
-{
-    int i = 0;
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
