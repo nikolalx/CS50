def77a502c9d1e70e88995f5e94279acd198c7bd
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-23 14:25:22 +0000

    Sun, Oct 23, 2022, 2:25 PM +00:00

--- uppercase.c
+++ uppercase.c
@@ -7,8 +7,16 @@ int main(void)
     string s = get_string("Before: ");
     printf("After: ");
 
-    for(i = 0, n = strlen(s); i < n; i++)
+    for(int i = 0, n = strlen(s); i < n; i++)
     {
-        
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
     }
+    printf("\n");
 }
