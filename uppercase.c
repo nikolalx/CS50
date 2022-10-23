519459f301b0cda0ca51443a2701252016cf00d0
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-23 14:35:02 +0000

    Sun, Oct 23, 2022, 2:35 PM +00:00

--- uppercase.c
+++ uppercase.c
@@ -10,14 +10,7 @@ int main(void)
 
     for(int i = 0, n = strlen(s); i < n; i++)
     {
-        if(islower(s[i]))
-        {
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+       printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
