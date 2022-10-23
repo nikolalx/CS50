b57732942f3a51a4a0b414f2412512e2c8c3ee03
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-23 14:56:08 +0000

    Sun, Oct 23, 2022, 2:56 PM +00:00

---
+++ exit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("missing input\n");
+        return 1;
+    }
+    printf("%s)
+
+}
