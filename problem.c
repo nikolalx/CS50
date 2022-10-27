733f24c53dbe2148abdd4abbaf2469a6945562fd
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-10-27 20:22:09 +0000

    Thu, Oct 27, 2022, 8:22 PM +00:00

--- problem.c
+++ problem.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<cs50.h>
-#include<ma
+#include<math.h>
+
 int main(void)
 {
     int n;
@@ -13,7 +14,7 @@ int main(void)
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < n / 3; j++)
         {
             printf("#");
         }
