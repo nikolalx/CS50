5520b4646ba7b94416caaf60b9ba22c64c925dd1
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-02 21:58:49 +0000

    Wed, Nov 2, 2022, 9:58 PM +00:00

--- problem.c
+++ problem.c
@@ -17,6 +17,10 @@ int main(void)
             printf("   #");
             printf("  ");
             printf("#");
+            for(int j = 0; j < n + 1; j++)
+            {
+                printf(")
+            }
     }
      printf("\n");
 }
