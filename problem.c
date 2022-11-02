b9f090fb244ba13764553831be995527d81907ae
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-02 21:51:48 +0000

    Wed, Nov 2, 2022, 9:51 PM +00:00

--- problem.c
+++ problem.c
@@ -14,8 +14,6 @@ int main(void)
 
     for(int i = 0; i < n / n; i++)
     {
-        for(int j = 0; j < n / n; j++)
-        {
             printf("   #");
             printf("  ");
             printf("#");
