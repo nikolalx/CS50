5551daa79dcfe64863131fe56695e5eefcc895fe
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-02 22:18:52 +0000

    Wed, Nov 2, 2022, 10:18 PM +00:00

--- mario.c
+++ mario.c
@@ -13,7 +13,7 @@ int main(void)
 
     for (int i = 0; i < n + 1; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n - 1; j++)
         {
             printf("#");
         }
