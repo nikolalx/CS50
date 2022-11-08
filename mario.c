616018b5867abf3268596e831ba42ae3f61669b2
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-08 17:27:40 +0000

    Tue, Nov 8, 2022, 5:27 PM +00:00

--- mario.c
+++ mario.c
@@ -1,19 +1,34 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <math.h>
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
 
 int main(void)
 {
     int n;
-        do
-        {
-            n = get_int("Size: ");
-        }
-    while (n < 1);
 
-    for (int i = 0; i < n; i++)
+    do
     {
-        for (int j = 0; j < n; j++)
+        n = get_int("Heigth: ");
+    }
+
+    while(n < 1);
+
+    for(int i = 1; i <= n; i++)
+    {
+
+        for(int j = n - i - 1; j >= 0; j--)
+        {
+            printf(" ");
+        }
+        for(int z = 0; z < i; z++)
+        {
+            printf("#");
+        }
+        for(int k = 0; k < 2; k++)
+        {
+            printf(" ");
+        }
+        for(int l = 0; l < i; l++)
         {
             printf("#");
         }
