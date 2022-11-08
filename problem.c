31d5f238191e076091a153115dc5e919d6608f9b
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-08 15:01:06 +0000

    Tue, Nov 8, 2022, 3:01 PM +00:00

--- problem.c
+++ problem.c
@@ -12,9 +12,9 @@ int main(void)
 
     while(n < 1);
 
-    for(int i = 8; i < n; i++)
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = n - n - 1; j < n; j++)
         {
             printf("#");
         }
