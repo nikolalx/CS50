5c554b0583a888760671ac636cb078f83b48f2c0
nikolalx <114953866+nikolalx@users.noreply.github.com>
2022-11-08 14:31:02 +0000

    Tue, Nov 8, 2022, 2:31 PM +00:00

--- mario.c
+++ mario.c
@@ -9,11 +9,11 @@ int main(void)
         {
             n = get_int("Size: ");
         }
-    while (n < 1);k
+    while (n < 1);
 
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n; j++)
         {
             printf("#");
         }
